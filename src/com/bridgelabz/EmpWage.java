8d82365726fb260d90c2d7a24f8425e392ed8d5a
amolmane7471 <maneamol3233@gmail.com>
2022-08-10 12:29:31 +0530

    [Amol]:added a UC2 for daily employee wage

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -5,11 +5,28 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Pgm");
 		int IS_FULLTIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs;
+		int empWage;
 		double empcheck=Math.floor(Math.random()*10)%2;
 		if(empcheck==IS_FULLTIME)
+			/*
+			 * UC1 : check employee attendance
+			 */
+		{
+			empHrs=8;
 			System.out.println("Emp is present");
+		}
 		else 
+		{
+			empHrs=0;
 			System.out.println("Emp is absent");
-
+		}
+		/*
+		 * UC2 : calculate daily employee wage
+		 */
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:"+empWage);
+		
 	}
 }
